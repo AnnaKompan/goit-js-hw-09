dfb1d215f84b461931eb5754f29d82e70f9ac643
AnnaKompan <annakompanch@gmail.com>
2023-02-06 22:39:17 +0200

    modified

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -17,8 +17,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (selectedDates[0] < Date.now()) {
+    if (selectedDates[0] <= Date.now()) {
       startBtn.disabled = true;
       window.alert('Please choose a date in the future');
     } else {
@@ -34,12 +33,16 @@ startBtn.addEventListener('click', onStart);
 
 function onStart() {
   startBtn.disabled = true;
-  setInterval(() => {
+  const intervalId = setInterval(() => {
     const now = Date.now();
     const countDownDate = fp.selectedDates[0];
     const deltaTime = countDownDate - now;
-    const timeComponents = convertMs(deltaTime);
-    updateTimer(timeComponents);
+    if (deltaTime < 0) {
+      clearInterval(intervalId);
+    } else {
+      const timeComponents = convertMs(deltaTime);
+      updateTimer(timeComponents);
+    }
   }, 1000);
 }
 
@@ -48,27 +51,18 @@ function pad(value) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = pad(Math.floor(ms / day));
-  // Remaining hours
   const hours = pad(Math.floor((ms % day) / hour));
-  // Remaining minutes
   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 function updateTimer({ days, hours, minutes, seconds }) {
   refs.timerDays.textContent = `${days}`;
